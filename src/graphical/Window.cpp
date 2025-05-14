c56b5b4cb8b7d15793b8b7efa088ff7dfa62cf05
Clément Lagier <clement.lagier@epitech.eu>
2025-05-15 07:33:03 +0900

    [UPDATE] Modified the Window.cpp file
    Added a temporary handling of events, to switch between the scenes, and load config from the config file

--- src/graphical/Window.cpp
+++ src/graphical/Window.cpp
@@ -4,7 +4,8 @@ graphical::Window::Window(const std::string &title, unsigned int width, unsigned
 {
   _window = std::make_shared<sf::RenderWindow>(sf::VideoMode({width, height}), title);
 
-  std::cout << "Creating window: " << title << std::endl;
+  Logging::Log("Window created with title: " + title);
+  Logging::Log("Window size: " + std::to_string(width) + "x" + std::to_string(height));
 }
 
 graphical::Window::~Window()
@@ -14,20 +15,46 @@ graphical::Window::~Window()
 
 void graphical::Window::loadConfig()
 {
-  // iterate through the config folder for .cfg files (CONFIG_FOLDER)
+  _sceneManager = std::make_shared<scene::SceneManager>();
+  _configLoader = std::make_shared<lib::ConfigLoader>();
+
   std::filesystem::path configPath = std::filesystem::current_path() / CONFIG_FOLDER;
   if (!std::filesystem::exists(configPath))
     throw WindowException(INVALID_CONFIG_FOLDER(configPath.string()));
 
   for (const auto &entry : std::filesystem::directory_iterator(configPath)) {
-    std::cout << "Found config file: " << entry.path() << std::endl;
+    if (entry.path().filename() == "scenes.cfg") {
+      Logging::Log("Loading scenes configuration file");
+
+      _configLoader->loadFile(entry.path().string());
+      _availableScenesConfigFile = _configLoader->getStringList("SCENES_FILES", ",");
+
+
+      for (const auto &scene : _availableScenesConfigFile) {
+        Logging::Log("Available scene: " + scene);
+        std::filesystem::path scenePath = configPath / scene;
+        if (!std::filesystem::exists(scenePath))
+          throw WindowException(INVALID_SCENE_FILE(scenePath.string()));
+
+        _sceneManager->addScene(std::make_shared<scene::Scene>(scenePath.string()));
+      }
+    }
+  }
+}
+
+void graphical::Window::initScenes()
+{
+  for (const auto &scene : _sceneManager->getSceneNames()) {
+    std::map<std::string, std::string> sceneConfig = _sceneManager->getScene(scene)->init();
+    _window->clear(sf::Color(_sceneManager->getScene(scene)->getConfig()->getBackgroundColor()));
   }
 }
 
 void graphical::Window::init()
 {
   loadConfig();
-
+  _sceneManager->setCurrentScene("menu");
+  initScenes();
 }
 
 void graphical::Window::run()
@@ -41,21 +68,32 @@ void graphical::Window::run()
 
 void graphical::Window::processEvents()
 {
+
   while (const std::optional event = _window->pollEvent()) {
-    if (event->is<sf::Event::Closed>()) {
-      _window->close();
-      std::cout << "Window closed" << std::endl;
-    }
+    event->visit([&](const auto& e) {
+      using T = std::decay_t<decltype(e)>;
+
+      if constexpr (std::is_same_v<T, sf::Event::Closed>) {
+          _window->close();
+      } else if constexpr (std::is_same_v<T, sf::Event::KeyPressed>) {
+        if (e.code == sf::Keyboard::Key::A) {
+          _sceneManager->setCurrentScene("menu_level");
+        } else if (e.code == sf::Keyboard::Key::B) {
+          _sceneManager->setCurrentScene("menu");
+        }
+      }
+    });
   }
 }
 
 void graphical::Window::update()
 {
-
+  // initScenes();
 }
 
 void graphical::Window::render()
 {
-  _window->clear();
+  _window->clear(sf::Color(_sceneManager->getCurrentScene()->getConfig()->getBackgroundColor()));
+  _sceneManager->getCurrentScene()->render();
   _window->display();
 }
